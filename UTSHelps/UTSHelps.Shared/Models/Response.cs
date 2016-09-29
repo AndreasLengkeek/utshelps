e5da2e3d66e0dec65997d2da8c71efb7e7233a18
AndreasLengkeek <andreas@propertybasecrm.com>
2016-09-29 19:57:20 +1000

    More conflicts

---
+++ UTSHelps/UTSHelps.Shared/Models/Response.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSHelps.Shared.Models
+{
+
+    public class Response<T>
+    {
+        public List<T> Results { get; set; }
+        public bool IsSuccess { get; set; }
+        public string DisplayMessage { get; set; }
+    }
+}
