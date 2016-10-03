e91dc224f644c0ab811f7f64257226fdfe4f57f1
AndreasLengkeek <andreas@propertybasecrm.com>
2016-10-03 23:23:56 +1100

    Add booking service

---
+++ UTSHelps/UTSHelps.Shared/Models/Booking.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTSHelps.Shared.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public int workshopId { get; set; }
+        public string studentId { get; set; }
+        public string topic { get; set; }
+        public string description { get; set; }
+        public string targetingGroup { get; set; }
+        public int campusID { get; set; }
+        public DateTime starting { get; set; }
+        public DateTime ending { get; set; }
+        public int maximum { get; set; }
+        public int? cutoff { get; set; }
+        public int? attended { get; set; }
+        public string type { get; set; }
+        public int WorkShopSetID { get; set; }
+        public string WorkShopSetName { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string notes { get; set; }
+    }
+}
---
+++ UTSHelps/UTSHelps.Shared/Services/BookingService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using UTSHelps.Shared.Helpers;
+using UTSHelps.Shared.Models;
+
+namespace UTSHelps.Shared.Services
+{
+    public class BookingService : HelpsService
+    {
+        public BookingService() : base()
+        {
+        }
+
+        public async Task<Response<Booking>> GetBookings(int studentId)
+        {
+            var queryString = "studentId=" + studentId;
+            var response = await helpsClient.GetAsync("api/workshop/booking/search?" + queryString);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsAsync<Response<Booking>>();
+                return result;
+            }
+            return ResponseHelper.CreateErrorResponse<Booking>("An unknown error occured");
+        }
+    }
+}
