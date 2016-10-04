0e0d117e7ed60b3a8b8fa3aeb977d927a6361f28
Trozza <troypoulter@gmail.com>
2016-10-04 11:50:54 +1100

    Add login screen UI

--- UTSHelps/UTSHelps.Droid/Resources/Resource.Designer.cs
+++ UTSHelps/UTSHelps.Droid/Resources/Resource.Designer.cs
@@ -44,6 +44,12 @@ namespace UTSHelps.Droid
 		public partial class Color
 		{
 			
+			// aapt resource value: 0x7f050004
+			public const int Helps_Red = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int Login_Text = 2131034117;
+			
 			// aapt resource value: 0x7f050002
 			public const int Side_Bar = 2131034114;
 			
@@ -52,6 +58,9 @@ namespace UTSHelps.Droid
 			
 			// aapt resource value: 0x7f050001
 			public const int Text_Gray = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int Transparent_White = 2131034115;
 			
 			static Color()
 			{
@@ -69,44 +78,47 @@ namespace UTSHelps.Droid
 			// aapt resource value: 0x7f040000
 			public const int center_align_element = 2130968576;
 			
-			// aapt resource value: 0x7f040005
-			public const int material_text_body1 = 2130968581;
-			
-			// aapt resource value: 0x7f040006
-			public const int material_text_body2 = 2130968582;
-			
 			// aapt resource value: 0x7f040002
-			public const int material_text_button = 2130968578;
-			
-			// aapt resource value: 0x7f040004
-			public const int material_text_caption = 2130968580;
-			
-			// aapt resource value: 0x7f04000a
-			public const int material_text_display1 = 2130968586;
-			
-			// aapt resource value: 0x7f04000b
-			public const int material_text_display2 = 2130968587;
-			
-			// aapt resource value: 0x7f04000c
-			public const int material_text_display3 = 2130968588;
-			
-			// aapt resource value: 0x7f04000d
-			public const int material_text_display4 = 2130968589;
-			
-			// aapt resource value: 0x7f040009
-			public const int material_text_headline = 2130968585;
-			
-			// aapt resource value: 0x7f040003
-			public const int material_text_menu = 2130968579;
-			
-			// aapt resource value: 0x7f040007
-			public const int material_text_subhead = 2130968583;
-			
-			// aapt resource value: 0x7f040008
-			public const int material_text_title = 2130968584;
+			public const int inside_lr = 2130968578;
 			
 			// aapt resource value: 0x7f040001
-			public const int spacing_element = 2130968577;
+			public const int inside_tb = 2130968577;
+			
+			// aapt resource value: 0x7f040006
+			public const int material_text_body1 = 2130968582;
+			
+			// aapt resource value: 0x7f040007
+			public const int material_text_body2 = 2130968583;
+			
+			// aapt resource value: 0x7f040003
+			public const int material_text_button = 2130968579;
+			
+			// aapt resource value: 0x7f040005
+			public const int material_text_caption = 2130968581;
+			
+			// aapt resource value: 0x7f04000b
+			public const int material_text_display1 = 2130968587;
+			
+			// aapt resource value: 0x7f04000c
+			public const int material_text_display2 = 2130968588;
+			
+			// aapt resource value: 0x7f04000d
+			public const int material_text_display3 = 2130968589;
+			
+			// aapt resource value: 0x7f04000e
+			public const int material_text_display4 = 2130968590;
+			
+			// aapt resource value: 0x7f04000a
+			public const int material_text_headline = 2130968586;
+			
+			// aapt resource value: 0x7f040004
+			public const int material_text_menu = 2130968580;
+			
+			// aapt resource value: 0x7f040008
+			public const int material_text_subhead = 2130968584;
+			
+			// aapt resource value: 0x7f040009
+			public const int material_text_title = 2130968585;
 			
 			static Dimension()
 			{
@@ -152,13 +164,19 @@ namespace UTSHelps.Droid
 			public const int login_logo = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int SignInBackground = 2130837514;
+			public const int rounded_login = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int tick = 2130837515;
+			public const int rounded_red_button = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int userAddIcon = 2130837516;
+			public const int SignInBackground = 2130837516;
+			
+			// aapt resource value: 0x7f02000d
+			public const int tick = 2130837517;
+			
+			// aapt resource value: 0x7f02000e
+			public const int userAddIcon = 2130837518;
 			
 			static Drawable()
 			{
@@ -173,191 +191,197 @@ namespace UTSHelps.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f090028
-			public const int Rego2BestContactNo = 2131296296;
+			// aapt resource value: 0x7f09002a
+			public const int Rego2BestContactNo = 2131296298;
 			
-			// aapt resource value: 0x7f090018
-			public const int adapterWorkshop = 2131296280;
+			// aapt resource value: 0x7f09001a
+			public const int adapterWorkshop = 2131296282;
 			
-			// aapt resource value: 0x7f090007
-			public const int bookingdate = 2131296263;
-			
-			// aapt resource value: 0x7f090006
-			public const int bookingsAdapter = 2131296262;
-			
-			// aapt resource value: 0x7f090019
-			public const int colorWorkshop = 2131296281;
+			// aapt resource value: 0x7f090009
+			public const int bookingdate = 2131296265;
 			
 			// aapt resource value: 0x7f090008
-			public const int date = 2131296264;
+			public const int bookingsAdapter = 2131296264;
 			
-			// aapt resource value: 0x7f09000c
-			public const int dateNtime = 2131296268;
-			
-			// aapt resource value: 0x7f09000d
-			public const int day = 2131296269;
+			// aapt resource value: 0x7f09001b
+			public const int colorWorkshop = 2131296283;
 			
 			// aapt resource value: 0x7f09000a
-			public const int detail_workshop = 2131296266;
+			public const int date = 2131296266;
+			
+			// aapt resource value: 0x7f09000e
+			public const int dateNtime = 2131296270;
+			
+			// aapt resource value: 0x7f09000f
+			public const int day = 2131296271;
+			
+			// aapt resource value: 0x7f09000c
+			public const int detail_workshop = 2131296268;
 			
 			// aapt resource value: 0x7f090002
 			public const int edit_toolbar = 2131296258;
 			
+			// aapt resource value: 0x7f090007
+			public const int forgotPassword = 2131296263;
+			
 			// aapt resource value: 0x7f090001
 			public const int fragmentContainer = 2131296257;
 			
-			// aapt resource value: 0x7f09001f
-			public const int imageView1 = 2131296287;
+			// aapt resource value: 0x7f090021
+			public const int imageView1 = 2131296289;
 			
-			// aapt resource value: 0x7f090023
-			public const int linearLayout1 = 2131296291;
+			// aapt resource value: 0x7f090025
+			public const int linearLayout1 = 2131296293;
 			
-			// aapt resource value: 0x7f09000f
-			public const int location = 2131296271;
+			// aapt resource value: 0x7f090011
+			public const int location = 2131296273;
 			
-			// aapt resource value: 0x7f090010
-			public const int locationName = 2131296272;
+			// aapt resource value: 0x7f090012
+			public const int locationName = 2131296274;
 			
-			// aapt resource value: 0x7f090005
-			public const int loginButton = 2131296261;
-			
-			// aapt resource value: 0x7f090004
-			public const int loginPassword = 2131296260;
+			// aapt resource value: 0x7f090006
+			public const int loginButton = 2131296262;
 			
 			// aapt resource value: 0x7f090003
-			public const int loginStudentId = 2131296259;
+			public const int loginLogo = 2131296259;
 			
-			// aapt resource value: 0x7f09001d
-			public const int lstCurrentBooking = 2131296285;
+			// aapt resource value: 0x7f090005
+			public const int loginPassword = 2131296261;
+			
+			// aapt resource value: 0x7f090004
+			public const int loginStudentId = 2131296260;
+			
+			// aapt resource value: 0x7f09001f
+			public const int lstCurrentBooking = 2131296287;
+			
+			// aapt resource value: 0x7f090020
+			public const int lstPastBooking = 2131296288;
+			
+			// aapt resource value: 0x7f090039
+			public const int lstSessions = 2131296313;
 			
 			// aapt resource value: 0x7f09001e
-			public const int lstPastBooking = 2131296286;
-			
-			// aapt resource value: 0x7f090037
-			public const int lstSessions = 2131296311;
-			
-			// aapt resource value: 0x7f09001c
-			public const int lstWorkshop = 2131296284;
+			public const int lstWorkshop = 2131296286;
 			
 			// aapt resource value: 0x7f090000
 			public const int main_content = 2131296256;
 			
-			// aapt resource value: 0x7f090039
-			public const int menu_add = 2131296313;
-			
-			// aapt resource value: 0x7f090038
-			public const int menu_bookings = 2131296312;
-			
 			// aapt resource value: 0x7f09003b
-			public const int menu_edit = 2131296315;
-			
-			// aapt resource value: 0x7f09003d
-			public const int menu_preferences = 2131296317;
-			
-			// aapt resource value: 0x7f09003c
-			public const int menu_save = 2131296316;
+			public const int menu_add = 2131296315;
 			
 			// aapt resource value: 0x7f09003a
-			public const int menu_settings = 2131296314;
+			public const int menu_bookings = 2131296314;
 			
-			// aapt resource value: 0x7f090009
-			public const int month = 2131296265;
+			// aapt resource value: 0x7f09003d
+			public const int menu_edit = 2131296317;
 			
-			// aapt resource value: 0x7f090012
-			public const int noOfSession = 2131296274;
+			// aapt resource value: 0x7f09003f
+			public const int menu_preferences = 2131296319;
 			
-			// aapt resource value: 0x7f090017
-			public const int placesLeft = 2131296279;
+			// aapt resource value: 0x7f09003e
+			public const int menu_save = 2131296318;
 			
-			// aapt resource value: 0x7f090022
-			public const int rego1StartButton = 2131296290;
-			
-			// aapt resource value: 0x7f09002a
-			public const int rego2Birthdate = 2131296298;
-			
-			// aapt resource value: 0x7f090029
-			public const int rego2Email = 2131296297;
-			
-			// aapt resource value: 0x7f090025
-			public const int rego2PreferredFIrstName = 2131296293;
-			
-			// aapt resource value: 0x7f090024
-			public const int rego2StudentName = 2131296292;
-			
-			// aapt resource value: 0x7f090026
-			public const int rego2homePhone = 2131296294;
-			
-			// aapt resource value: 0x7f090027
-			public const int rego2mobilePhone = 2131296295;
-			
-			// aapt resource value: 0x7f090030
-			public const int rego3Course = 2131296304;
-			
-			// aapt resource value: 0x7f090031
-			public const int rego3DegreeList = 2131296305;
-			
-			// aapt resource value: 0x7f09002f
-			public const int rego3Faculty = 2131296303;
-			
-			// aapt resource value: 0x7f090032
-			public const int rego3YearList = 2131296306;
-			
-			// aapt resource value: 0x7f090033
-			public const int rego4AddBtn = 2131296307;
-			
-			// aapt resource value: 0x7f090035
-			public const int rego5ContinueBtn = 2131296309;
-			
-			// aapt resource value: 0x7f09002c
-			public const int rego5CountryList = 2131296300;
-			
-			// aapt resource value: 0x7f09002b
-			public const int rego5GenderList = 2131296299;
-			
-			// aapt resource value: 0x7f09002d
-			public const int rego5LanguageList = 2131296301;
-			
-			// aapt resource value: 0x7f090034
-			public const int rego5NewBookingBtn = 2131296308;
-			
-			// aapt resource value: 0x7f09002e
-			public const int rego5StatusList = 2131296302;
-			
-			// aapt resource value: 0x7f090014
-			public const int sessionDate = 2131296276;
-			
-			// aapt resource value: 0x7f090015
-			public const int sessionLocation = 2131296277;
-			
-			// aapt resource value: 0x7f090011
-			public const int sessionName = 2131296273;
-			
-			// aapt resource value: 0x7f090016
-			public const int sessionPlace = 2131296278;
-			
-			// aapt resource value: 0x7f090013
-			public const int sessionTime = 2131296275;
-			
-			// aapt resource value: 0x7f090036
-			public const int session_workshopName = 2131296310;
-			
-			// aapt resource value: 0x7f090020
-			public const int textView1 = 2131296288;
-			
-			// aapt resource value: 0x7f090021
-			public const int textView2 = 2131296289;
-			
-			// aapt resource value: 0x7f09000e
-			public const int time = 2131296270;
-			
-			// aapt resource value: 0x7f09001b
-			public const int toolbar = 2131296283;
+			// aapt resource value: 0x7f09003c
+			public const int menu_settings = 2131296316;
 			
 			// aapt resource value: 0x7f09000b
-			public const int txtWorkshop = 2131296267;
+			public const int month = 2131296267;
 			
-			// aapt resource value: 0x7f09001a
-			public const int workshopSets_text = 2131296282;
+			// aapt resource value: 0x7f090014
+			public const int noOfSession = 2131296276;
+			
+			// aapt resource value: 0x7f090019
+			public const int placesLeft = 2131296281;
+			
+			// aapt resource value: 0x7f090024
+			public const int rego1StartButton = 2131296292;
+			
+			// aapt resource value: 0x7f09002c
+			public const int rego2Birthdate = 2131296300;
+			
+			// aapt resource value: 0x7f09002b
+			public const int rego2Email = 2131296299;
+			
+			// aapt resource value: 0x7f090027
+			public const int rego2PreferredFIrstName = 2131296295;
+			
+			// aapt resource value: 0x7f090026
+			public const int rego2StudentName = 2131296294;
+			
+			// aapt resource value: 0x7f090028
+			public const int rego2homePhone = 2131296296;
+			
+			// aapt resource value: 0x7f090029
+			public const int rego2mobilePhone = 2131296297;
+			
+			// aapt resource value: 0x7f090032
+			public const int rego3Course = 2131296306;
+			
+			// aapt resource value: 0x7f090033
+			public const int rego3DegreeList = 2131296307;
+			
+			// aapt resource value: 0x7f090031
+			public const int rego3Faculty = 2131296305;
+			
+			// aapt resource value: 0x7f090034
+			public const int rego3YearList = 2131296308;
+			
+			// aapt resource value: 0x7f090035
+			public const int rego4AddBtn = 2131296309;
+			
+			// aapt resource value: 0x7f090037
+			public const int rego5ContinueBtn = 2131296311;
+			
+			// aapt resource value: 0x7f09002e
+			public const int rego5CountryList = 2131296302;
+			
+			// aapt resource value: 0x7f09002d
+			public const int rego5GenderList = 2131296301;
+			
+			// aapt resource value: 0x7f09002f
+			public const int rego5LanguageList = 2131296303;
+			
+			// aapt resource value: 0x7f090036
+			public const int rego5NewBookingBtn = 2131296310;
+			
+			// aapt resource value: 0x7f090030
+			public const int rego5StatusList = 2131296304;
+			
+			// aapt resource value: 0x7f090016
+			public const int sessionDate = 2131296278;
+			
+			// aapt resource value: 0x7f090017
+			public const int sessionLocation = 2131296279;
+			
+			// aapt resource value: 0x7f090013
+			public const int sessionName = 2131296275;
+			
+			// aapt resource value: 0x7f090018
+			public const int sessionPlace = 2131296280;
+			
+			// aapt resource value: 0x7f090015
+			public const int sessionTime = 2131296277;
+			
+			// aapt resource value: 0x7f090038
+			public const int session_workshopName = 2131296312;
+			
+			// aapt resource value: 0x7f090022
+			public const int textView1 = 2131296290;
+			
+			// aapt resource value: 0x7f090023
+			public const int textView2 = 2131296291;
+			
+			// aapt resource value: 0x7f090010
+			public const int time = 2131296272;
+			
+			// aapt resource value: 0x7f09001d
+			public const int toolbar = 2131296285;
+			
+			// aapt resource value: 0x7f09000d
+			public const int txtWorkshop = 2131296269;
+			
+			// aapt resource value: 0x7f09001c
+			public const int workshopSets_text = 2131296284;
 			
 			static Id()
 			{
